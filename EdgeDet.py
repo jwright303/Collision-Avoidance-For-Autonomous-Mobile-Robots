4468708db95e8b5c5b6998b633564ea87358c208
Braden Lee <55996492+0x00C0DE@users.noreply.github.com>
2022-02-23 17:32:14 -0800

    Add files via upload
    Updated edgeDet to better detect cliffs/major edges in a image/frame

--- EdgeDet.py
+++ EdgeDet.py
@@ -3,25 +3,112 @@ import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
 import PIL
+import diplib as dip
 
-img = cv2.imread("myrx8_2.jpg", cv2.IMREAD_GRAYSCALE)
+img_orig = cv2.imread("cliff_front.png", cv2.IMREAD_GRAYSCALE)
+
+# (image [contrast]) --begin--
+img = cv2.imread("cliff_front.png", 1)
+
+#--Reading the image---
+#cv2.imshow("img",img) 
+
+#--Converting image to LAB-- 
+lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+#cv2.imshow("lab",lab)
+
+#--Splitting LAB image to different channels--
+l, a, b = cv2.split(lab)
+#cv2.imshow('l_channel', l)
+#cv2.imshow('a_channel', a)
+#cv2.imshow('b_channel', b)
+
+#--Applying CLAHE to L-channel--
+clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
+cl = clahe.apply(l)
+#cv2.imshow('CLAHE output', cl)
+
+#--Merge the CLAHE enhanced L-channel with the a and b channel--
+limg = cv2.merge((cl,a,b))
+#cv2.imshow('limg', limg)
+
+#--Converting image from LAB to RGB--
+final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+cv2.imshow('final', final)
+
+# saves the image to the current working directory
+filName = 'canny_final_contrast.png'
+cv2.imwrite(filName, final)
+
+# image contrast --end--
+
+
+
+img3 = cv2.imread("canny_final_contrast.png")
+
+
+# line detection using hough transform --begin--
+img_hough = cv2.imread("cliff_front.png")
+gray = cv2.cvtColor(img_hough, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(final, 70, 390)
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 30, maxLineGap=1)
+for line in lines:
+   x1, y1, x2, y2 = line[0]
+   cv2.line(img_hough, (x1, y1), (x2, y2), (0, 0, 256), 1)
+
+filName = 'hough_transform.png'
+cv2.imwrite(filName, img_hough)
+
+# hough transform --end--
 
 # parameters to adjust the threshold for calculating major edges in the image
-edges = cv2.Canny(img,350,350)
+#gray_image = cv2.cvtColor(img_orig, cv2.COLOR_RGB2GRAY)
+blur_image = cv2.GaussianBlur(img_orig, (5, 5), 2)
 
-titles = ['image', 'Canny']
-images = [img, edges]
+
+#Creates canny edge detection image of the camera frame
+
+# front spec
+#canny = cv2.Canny(blur_image, 15, 240, apertureSize=3)
+
+# back spec
+canny = cv2.Canny(blur_image, 15, 190, apertureSize=3)
+
+filName = 'canny2.png'
+cv2.imwrite(filName, canny)
+
+#culled edges threshold
+min_threshold = 250
+culled_edge = dip.BinaryAreaOpening(canny > 100, min_threshold)
+
+#converts dip.image back to numpy array
+#culled_edge = np.array(culled_edge)
+
+(contours, _) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+outlines = img_orig.copy()
+cv2.drawContours(outlines, contours, -1, (255, 0, 0), 2)
+
+#applies contours to bounding edges detected
+for contour in contours:
+    x,y,w,h = cv2.boundingRect(contour)
+    cv2.rectangle(outlines,(x,y),(x+w,y+h),(0,255,0),2)
+
+#cv2.imshow('canny edge detect', outlines)
+cv2.imshow('outlines', outlines)
+
+titles = ['image', 'hough transform', 'outlines','Canny', 'culled_edge']
+images = [img_orig,  img_hough, outlines, canny, culled_edge]
 
 # Loop to display the images corresponding to their title
-for i in range(2):
-    plt.subplot(1, 2, i+1), plt.imshow(images[i], 'gray')
+for i in range(5):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i])
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
 
-plt.show()
+curb1 = plt.show()
 
 
-# saves the image to the current working directory
-filName = 'canny1.jpg'
-cv2.imwrite(filName, edges)
+
+
 
